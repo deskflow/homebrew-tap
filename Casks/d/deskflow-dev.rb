daf14a4ae27ca4568c7d73d199a7e5f666eceb78
sithlord48 <sithlord48@gmail.com>
2024-11-06 16:20:04 -0500

    add cron to update dev package

--- Casks/d/deskflow-dev.rb
+++ Casks/d/deskflow-dev.rb
@@ -2,8 +2,7 @@ cask "deskflow-dev" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.17.0.176"
-  sha256 arm:   "4e419934d33f68e6bc9d240e47d1f3b3fd5e716375c8bfc8d996f0045cf9acf5",
-         intel: "35583121c89e9b7ae1fcaf010f982876e49502f828609d0b9c225634c40a7e55"
+  sha256 arm:   "4e419934d33f68e6bc9d240e47d1f3b3fd5e716375c8bfc8d996f0045cf9acf5", intel: "35583121c89e9b7ae1fcaf010f982876e49502f828609d0b9c225634c40a7e55"
 
   url "https://github.com/deskflow/deskflow/releases/download/continuous/deskflow-continuous-macos-#{arch}.dmg",
       verified: "github.com/deskflow/deskflow/"
