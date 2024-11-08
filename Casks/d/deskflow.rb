1009163089b057408fc57f674960e2e8fbb2a32d
sithlord48 <sithlord48@gmail.com>
2024-11-08 07:35:18 -0500

    deskflow: 1.17.1

--- Casks/d/deskflow.rb
+++ Casks/d/deskflow.rb
@@ -1,11 +1,11 @@
 cask "deskflow" do
-  arch arm: "arm64", intel: "x64"
+  arch arm: "arm64", intel: "x86_64"
 
-  version "1.17.0"
-  sha256 arm:   "cf0421257bb5c1ae1c14ec1f470c119619527e0b1f1b7f9fa1e66213c6bc88f4",
-         intel: "64fc270052c31fe0843c1c1374ccfefe0d646bd7abf600ca3a9bd978dab4ed88"
+  version "1.17.1"
+  sha256 arm:   "11c243989d322b3b4b53a086fb89ccbcd64540f82fd3ea95ee600802999a13e3",
+         intel: "079d047d1660931f2f6e7f151c58650e601a6cfab5f3d3bb3948ec2b09ffe728"
 
-  url "https://github.com/deskflow/deskflow/releases/download/v#{version}/deskflow-#{version}.0_mac_#{arch}.dmg",
+  url "https://github.com/deskflow/deskflow/releases/download/v#{version}/deskflow-#{version}-macos-#{arch}.dmg",
       verified: "github.com/deskflow/deskflow/"
   name "Deskflow"
   desc "Mouse and keyboard sharing utility"
