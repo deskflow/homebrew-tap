c5d97d28d97b014199e650253cfa990e85c090e3
sithlord48 <sithlord48@users.noreply.github.com>
2025-06-18 13:16:00 +0000

    deskflow-dev: 1.22.0.71

--- Casks/d/deskflow-dev.rb
+++ Casks/d/deskflow-dev.rb
@@ -1,8 +1,8 @@
 cask "deskflow-dev" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.22.0.70"
-  sha256 arm:   "9f6d378a4752dbb8465899d3105c4e05d4ba0f71b7d554e5dbd37aa839fcc9ee", intel: "6c5cc9094ae786516646315efdf5ba54715e1daa31271243d143b3457154035a"
+  version "1.22.0.71"
+  sha256 arm:   "181f89738404e2f7c417a1d14f091b4f9201d1fc87cfd895b2170576a97bbe9c", intel: "fe2f5439ca1c5740aff83b18bafd76da69835f9f72fa3d3b1a411f7cc1ea4d19"
 
   url "https://github.com/deskflow/deskflow/releases/download/continuous/deskflow-continuous-macos-#{arch}.dmg",
       verified: "github.com/deskflow/deskflow/"
